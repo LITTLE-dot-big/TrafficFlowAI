df3b64a075abe3b7405a4ad334dbbb717e7b201c
Amandeep Kumar <amanequalskul@outlook.com>
2023-08-06 23:18:44 +0530

    Update A1.py

--- A1.py
+++ A1.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 import cv2 as cv
 import cvzone as cvn
-# from sort import *
+from sort import *
 import numpy as np
 
 cap = cv.VideoCapture('videos/testVideo.mp4')   # loading the video stream
 mask = cv.imread('videos/mask.png')     # loading the mask for detection in the particular region
-# track = Sort(max_age=15, min_hits=3, iou_threshold=0.3)     # a program for tracking the detected vechiles
+track = Sort(max_age=15, min_hits=3, iou_threshold=0.3)     # a program for tracking the detected vechiles
 limits = [0, 300, 800, 300]
 cntBus = []
 cntCar = []
@@ -19,15 +19,10 @@ classNames = model.names
 
 while True:
     _, img = cap.read()
-    # cv.imshow('img', img)
-    # cv.waitKey(1)
     # making the video stream compatible for detection 
     imgMask = cv.bitwise_and(img, mask)
-    # cv.imshow(imgMask)
-    # cv.waitKey(0)
     result = model(imgMask, stream=True)
     detected = np.empty((0, 5))
-    # print(type(result))
     for r in result:
         box = r.boxes
         for b in box:
@@ -44,35 +39,35 @@ while True:
                                 offset=3, scale=0.7, thickness=1)
                 currArr = np.array((x1, y1, x2, y2, prob)) 
                 detected = np.vstack((detected, currArr))
-    # resultTrack = track.update(detected)
+    resultTrack = track.update(detected)
     cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
-    # for r in resultTrack:
-    #     x1, y1, x2, y2, id = r 
-    #     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-    #     w, h = x2-x1, y2-y1
-    #     cvn.cornerRect(img, (x1, y1, w, h), l=10, rt=2, colorR=(0, 255, 0))
-        # cvn.putTextRect(img, f'{id}', (max(0, x1), max(40, y1)),
-    #     #                         offset=10, scale=2 , thickness=3)
-    #     cx, cy = x1+w//2, y1+h//2
-    #     cv.circle(img, (cx, cy), 5, (255, 0, 255), cv.FILLED)
-    #     # counting the numbers of each type of vechiles. Classified vehicular flow
-    #     if limits[0]<cx<limits[2] and limits[1]-15<cy<limits[3]+15:
-    #         if cntBus.count(id) == 0:
-    #             cntBus.append(id)
-    #             cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
-    #         if cntCar.count(id) == 0:
-    #             cntCar.append(id)
-    #             cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
-    #         if cntTruck.count(id) == 0:
-    #             cntTruck.append(id)
-    #             cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
-    #         if cntBike.count(id) == 0:
-    #             cntBike.append(id)
-    #             cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
-    #     cvn.putTextRect(img, f'Count: {len(cntBus)}', (50, 50), scale=1, thickness=1)
-    #     cvn.putTextRect(img, f'Count: {len(cntTruck)}', (200, 50), scale=1, thickness=1)
-    #     cvn.putTextRect(img, f'Count: {len(cntCar)}', (400, 50), scale=1, thickness=1)
-    #     cvn.putTextRect(img, f'Count: {len(cntBike)}', (600, 50), scale=1, thickness=1)
+    for r in resultTrack:
+        x1, y1, x2, y2, id = r 
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2-x1, y2-y1
+        cvn.cornerRect(img, (x1, y1, w, h), l=10, rt=2, colorR=(0, 255, 0))
+        cvn.putTextRect(img, f'{id}', (max(0, x1), max(40, y1)),
+        #                         offset=10, scale=2 , thickness=3)
+        cx, cy = x1+w//2, y1+h//2
+        cv.circle(img, (cx, cy), 5, (255, 0, 255), cv.FILLED)
+        # counting the numbers of each type of vechiles. Classified vehicular flow
+        if limits[0]<cx<limits[2] and limits[1]-15<cy<limits[3]+15:
+            if cntBus.count(id) == 0:
+                cntBus.append(id)
+                cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
+            if cntCar.count(id) == 0:
+                cntCar.append(id)
+                cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
+            if cntTruck.count(id) == 0:
+                cntTruck.append(id)
+                cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
+            if cntBike.count(id) == 0:
+                cntBike.append(id)
+                cv.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
+        cvn.putTextRect(img, f'Count: {len(cntBus)}', (50, 50), scale=1, thickness=1)
+        cvn.putTextRect(img, f'Count: {len(cntTruck)}', (200, 50), scale=1, thickness=1)
+        cvn.putTextRect(img, f'Count: {len(cntCar)}', (400, 50), scale=1, thickness=1)
+        cvn.putTextRect(img, f'Count: {len(cntBike)}', (600, 50), scale=1, thickness=1)
     # showing the detection 
     cv.imshow("Image", imgMask)
     cv.imshow("Img", img)
